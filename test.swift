3d2fceb7a11d58c96583fc9f0e5bfd338d3ba397
alexey1312 <alexey1312ru@gmail.com>
2026-08-08 09:12:44 +0500

    chore: drop a scratch file the pre-commit hook swept into the merge

--- test.swift
+++
@@ -1,9 +0,0 @@
-import SwiftUI
-
-struct TestView: View {
-    @State var obj: Int
-    init() {
-        _obj = State(initialValue: { print("EVALUATED"); return 5 }())
-    }
-    var body: some View { Text("Hello") }
-}
